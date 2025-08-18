035e8a85efa229b2b9e2072f311502743423f9e3
Beutife <biholuwatisefeh@gmail.com>
2025-08-18 22:32:54 +0100

    vote

--- contracts/Voters.sol
+++ contracts/Voters.sol
@@ -16,7 +16,7 @@ contract VotingSystem{
    mapping (uint => Canditate) public candidates;
    mapping(address => bool) public hasVoted;
 
-   enum ElectionState{Notstarter, Ongoing, Ended}
+   enum ElectionState{Notstarted, Ongoing, Ended}
    ElectionState public state;
 
    constructor(){
@@ -28,6 +28,38 @@ contract VotingSystem{
     require(msg.sender == admin, "Not Admin")
    }
 
-   function addCandidate()
+   function addCandidate(string memory _name)public onlyAdmin{
+     canditatesCount++;
+     candidates[canditatesCount] = Canditate(canditatesCount, _name, 0, voted)
+   }
 
+   function startElection() public onlyAdmin{
+      require(state == ElectionState.Notstarted, 'Already Started')
+      state = ElectionState.Ongoing;
+   }
+
+   function vote(uint _candidateId) public{
+     require(state == ElectionState.Ongoing, 'Election not active');
+     require(!hasVoted[msg.sender], 'Already Voted');
+     require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
+
+     hasVoted[msg.sender] = true;
+     candidates[_candidateId].voteCount++;
+   }
+
+   function endELection() public onlyAdmin {
+      require(state == ElectionState.Ongoing, "Not Active");
+      state = ElectionState.Ended
+   }
+
+   function getWinner()public view returns(string memory winnerName){
+     require(state = ElectionState.Ended, "Election not ended");
+
+     uint winningVoteCount =0;
+     uint winnerId = 0;
+
+     for(uint i=1; i <= candidatesCount; i++){
+        
+     }
+   }
 }
