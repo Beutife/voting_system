a74e9d6a213cf3293e1aa5a4190a5fa265f522de
Beutife <biholuwatisefeh@gmail.com>
2025-08-25 00:43:05 +0100

    compiled contract

--- contracts/Voters.sol
+++ contracts/Voters.sol
@@ -1,69 +1,70 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract VotingSystem{
+contract VotingSystem {
     
     address public admin;
-    uint public canditatesCount;
+    uint public candidatesCount;
 
-    struct Canditate {
+    struct Candidate {
         string name;
         uint id;
         uint voteCount;
-        bool voted;
     }
 
-   mapping (uint => Canditate) public candidates;
-   mapping(address => bool) public hasVoted;
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public hasVoted;
 
-   enum ElectionState{Notstarted, Ongoing, Ended}
-   ElectionState public state;
+    enum ElectionState { Notstarted, Ongoing, Ended }
+    ElectionState public state;
 
-   constructor(){
-    admin = msg.sender;
-    state = ElectionState
-   }
+    constructor() {
+        admin = msg.sender;
+        state = ElectionState.Notstarted;
+    }
    
-   modifier onlyAdmin(){
-    require(msg.sender == admin, "Not Admin")
-   }
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not Admin");
+        _;
+    }
 
-   function addCandidate(string memory _name)public onlyAdmin{
-     canditatesCount++;
-     candidates[canditatesCount] = Canditate(canditatesCount, _name, 0, voted)
-   }
+    function addCandidate(string memory _name) public onlyAdmin {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(_name, candidatesCount, 0);
+    }
 
-   function startElection() public onlyAdmin{
-      require(state == ElectionState.Notstarted, 'Already Started')
-      state = ElectionState.Ongoing;
-   }
+    function startElection() public onlyAdmin {
+        require(state == ElectionState.Notstarted, "Already Started");
+        state = ElectionState.Ongoing;
+    }
 
-   function vote(uint _candidateId) public{
-     require(state == ElectionState.Ongoing, 'Election not active');
-     require(!hasVoted[msg.sender], 'Already Voted');
-     require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
+    function vote(uint _candidateId) public {
+        require(state == ElectionState.Ongoing, "Election not active");
+        require(!hasVoted[msg.sender], "Already Voted");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
 
-     hasVoted[msg.sender] = true;
-     candidates[_candidateId].voteCount++;
-   }
+        hasVoted[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
 
-   function endELection() public onlyAdmin {
-      require(state == ElectionState.Ongoing, "Not Active");
-      state = ElectionState.Ended
-   }
+    function endElection() public onlyAdmin {
+        require(state == ElectionState.Ongoing, "Not Active");
+        state = ElectionState.Ended;
+    }
 
-   function getWinner()public view returns(string memory winnerName){
-     require(state = ElectionState.Ended, "Election not ended");
+    function getWinner() public view returns(string memory winnerName) {
+        require(state == ElectionState.Ended, "Election not ended");
 
-     uint winningVoteCount =0;
-     uint winnerId = 0;
+        uint winningVoteCount = 0;
+        uint winnerId = 0;
 
-     for(uint i=1; i <= candidatesCount; i++){
-        if(candidates[i].voteCount > winningVoteCount){
-            winningVoteCount = candidates[i].voteCount;
-            winnerId =i;
+        for(uint i = 1; i <= candidatesCount; i++) {
+            if(candidates[i].voteCount > winningVoteCount) {
+                winningVoteCount = candidates[i].voteCount;
+                winnerId = i;
+            }
         }
-     }
-     winnerName = candidates[winnerId].name;
-   }
+
+        winnerName = candidates[winnerId].name;
+    }
 }
